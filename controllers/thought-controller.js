e8525a15d85b6db1bfcb08d3f994bd8295af4fb6
jessica-calderon <calderonjessica13@yahoo.com>
2022-08-27 01:18:13 -0500

    start controllers, add pseudocomments

---
+++ controllers/thought-controller.js
@@ -0,0 +1,56 @@
+const { Thought, User } = require("../models");
+
+const thoughtController = {
+    //get all thoughts
+
+    // get one thought by id
+  // add thought to User
+  addThought({ params, body }, res) {
+    console.log(body);
+    Thought.create(body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: params.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  // update thought by id
+  // remove thought
+  removeThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.thoughtId })
+      .then((deletedThought) => {
+        if (!deletedThought) {
+          return res.status(404).json({ message: "No thought with this id!" });
+        }
+        return User.findOneAndUpdate(
+          { _id: params.userId },
+          { $pull: { thoughts: params.thoughtId } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+};
+
+// create reactions
+// delete a reaction
+
+module.exports = thoughtController;
